53db5903ef4f4949f5106e015daf0768171aea61
sculove <sculove.pe.kr@navercorp.com>
2018-10-18 22:00:00 +0900

    init

---
+++ src/server/store.js
@@ -0,0 +1,5 @@
+const store = {
+  id: 10
+};
+
+export default store;
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'base',
+  mode: 'development',
+  entry: './foo.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'foo.bundle.js'
+  }
+}
